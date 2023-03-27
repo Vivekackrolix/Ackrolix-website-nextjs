b3508e9b8fedfe325357d3006c69e12ae62afcf8
boparaiamrit <boparaiamrit@gmail.com>
2023-03-27 22:18:31 +0530

    Path Changed and Updated

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Header from "../components/Common/Header";
 import Footer from "../components/Common/Footer";
-import PortfolioListing from "../components/portfolio/PortfolioListing";
-import PortfolioTopHeader from "../components/portfolio/PortfolioTopHeader";
+import PortfolioListing from "../components/Portfolio/PortfolioListing";
+import PortfolioTopHeader from "../components/Portfolio/PortfolioTopHeader";
 import Head from "next/head";
 function portfolio() {
   return (
