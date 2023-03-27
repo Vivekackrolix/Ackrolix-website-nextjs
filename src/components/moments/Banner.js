3f145b2820a7ce6c4c92cf5a4ed04843f21957e3
GaganVashishth <gagan.akrolix@gmail.com>
2023-03-27 12:12:57 +0530

    moment banner

--- src/components/moments/Banner.js
+++ src/components/moments/Banner.js
@@ -9,7 +9,7 @@ const Banner = () => {
             <div className="md:text-9xl text-center md:text-left text-[3rem] text-white tracking-[0.5rem] texttransparent font-bold  md:pt-10 ">
               Memories
             </div>
-            <div className="px-4">
+            <div className="px-2">
               <div className="text-3xl font-bold absolute md:top-[5rem] top-[3rem] md:left-2 left-[4rem] text-center md:text-left">
                 Our Memories
               </div>
@@ -21,6 +21,15 @@ const Banner = () => {
             </div>
           </div>
         </div>
+        {/* images section start */}
+        <div class="grid grid-cols-4 gap-4 p-5">
+          <div class="shadow-lg  p-10 rounded-lg row-span-2">1</div>
+          <div class="shadow-lg p-10 rounded-lg">2</div>
+          <div class="shadow-lg p-10 rounded-lg row-span-2">3</div>
+          <div class="shadow-lg p-10 rounded-lg">4</div>
+          <div class="shadow-lg p-10 rounded-lg col-span-3">5</div>
+        </div>
+        {/* images section end */}
       </div>
     </>
   );
