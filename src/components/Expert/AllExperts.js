7b07621aef8822dfea73c28529e653b59b6354b9
Aakif <akifmansoori8287@gmail.com>
2023-05-08 13:18:50 +0530

    ChangesInPortfolio

--- src/components/Expert/AllExperts.js
+++ src/components/Expert/AllExperts.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import React from "react";
-import { FaFacebookF, FaTwitter } from "react-icons/fa";
 
 const All = () => {
   const advisors = [
@@ -46,59 +45,36 @@ const All = () => {
       designation: "CTO",
       desc: "Everything you’ve ever wanted is on the other side of fear.",
     },
-    {
-      id: 7,
-      image: "/assets/images/advisorone.png",
-      name: "Macauley Herring",
-      designation: "CTO",
-      desc: "Everything you’ve ever wanted is on the other side of fear.",
-    },
-    {
-      id: 8,
-      image: "/assets/images/advisorone.png",
-      name: "Macauley Herring",
-      designation: "CTO",
-      desc: "Everything you’ve ever wanted is on the other side of fear.",
-    },
-    {
-      id: 9,
-      image: "/assets/images/advisorone.png",
-      name: "Macauley Herring",
-      designation: "CTO",
-      desc: "Everything you’ve ever wanted is on the other side of fear.",
-    },
   ];
   return (
     <div className="container-ack">
-      <div className="grid md:grid-cols-3 md:grid-rows-3 gap-3 pb-10">
-        {advisors.map((item,index) => (
+      <div className="grid md:grid-cols-3 gap-3 pb-10">
+        {advisors.map((item, index) => (
           <>
             <div className="group p-6" key={index}>
               <div className="relative text-center py-6 border-2 border-[#F6F8F9] duration duration-200 group-hover:-translate-y-2  hover:drop-shadow-sm hover:bg-[#F6F8F9]/40 hover:border-2 rounded-xl md:h-[21rem]">
-                <div className="inline-flex justify-center items-center rounded-md md:mt-8 ">
+                <div className="inline-flex justify-center items-center rounded-md md:mt-8">
                   <img
                     src={item.image}
                     alt=""
-                    className="  rounded-lg w-20 h-20 mx-auto "
+                    className="rounded-lg w-20 h-20 mx-auto"
                   />
                 </div>
 
                 <h2 className="font-semibold text-lg py-3 text-black text-center">
                   {item.name}
                 </h2>
-                <p className="text-primary   pb-4  text-center px-3 text-sm ">
+                <p className="text-primary pb-4 text-center px-3 text-sm">
                   {item.designation}
                 </p>
-                <p className="text-black/50 group-hover:text-black   text-center px-3 text-sm ">
+                <p className="text-black/50 group-hover:text-black text-center px-3 text-sm">
                   {item.desc}
                 </p>
-
-               
               </div>
             </div>
           </>
         ))}
-      </div> 
+      </div>
     </div>
   );
 };
--- src/components/Portfolio/PortfolioListing.js
+++ src/components/Portfolio/PortfolioListing.js
@@ -33,11 +33,10 @@ function PortfolioListing() {
             <div onClick={() => setSelect(item.id)} className="group  " key={e}>
               {" "}
               <div
-                className={` text-center py-2 rounded-md ${
-                  select == item.id
-                    ? "bg-secondary text-white border-2 "
-                    : "   border-2 border-transparent"
-                }`}
+                className={` text-center py-2 rounded-md ${select == item.id
+                  ? "bg-secondary text-white border-2 "
+                  : "   border-2 border-transparent"
+                  }`}
               >
                 {item.text}
               </div>
@@ -115,189 +114,201 @@ function PortfolioListing() {
             </>
           ) : select == 2 ? (
             <>
-              <div className="PortifloClum">
-                <div className="Protfolicol-12">
-                  <div className="col4RptProfolio">
-                    <div className="BacimgDtls">
-                      <img
-                        alt="Lngo"
-                        src="/assets/images/protfolio/fornt-protfilo.webp"
-                      />
+              <div className="container-ack gradientbackgroundone px-12 pt-12">
+                <div className=" grid grid-cols-3 justify-between ">
+                  <div className="col-span-2">
+                    <h2 className="text-xl font-semibold ">
+                      The standard Lorem Ipsum passage, used since the 1500s
+                    </h2>
+                    <p className="">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.
+                    </p>
+                  </div>
+                  <div>
+                    <div className="flex justify-end pb-10 col-span-1">
+                      <Link href="#">
+                        <button className=" button-2  px-7 py-2 rounded-md">
+                          <div className="eff-2"></div>
+                          <span className="text-base flex items-center ">
+                            {" "}
+                            View All
+                            <MdKeyboardArrowRight className=" text-base" />
+                          </span>
+                        </button>
+                      </Link>
                     </div>
-                    <div className="BxShow">
-                      <div className="BxMnvrSwn">
-                        <h5> Lorem Ipsum </h5>
-                        <Link className="text-primary" href={"/"}>
-                          {" "}
-                          Website Design{" "}
-                          <MdKeyboardArrowRight className=" text-base text-white inline-block" />
+                  </div>
+                </div>
+                <div>
+                  <img src="/assets/images/portfolio/portfoliomile.png" />
+                </div>
+              </div>
+              <div className="mt-12">
+                <div className="container-ack gradientbackgroundtwo px-12 pt-12 ">
+                  <div className=" grid grid-cols-3 justify-between ">
+                    <div className="col-span-2">
+                      <h2 className="text-xl font-semibold ">
+                        The standard Lorem Ipsum passage, used since the 1500s
+                      </h2>
+                      <p className="">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua.
+                      </p>
+                    </div>
+                    <div>
+                      <div className="flex justify-end pb-10 col-span-1">
+                        <Link href="#">
+                          <button className=" button-2  px-7 py-2 rounded-md">
+                            <div className="eff-2"></div>
+                            <span className="text-base flex items-center ">
+                              {" "}
+                              View All
+                              <MdKeyboardArrowRight className=" text-base" />
+                            </span>
+                          </button>
                         </Link>
                       </div>
                     </div>
                   </div>
-
-                  <div className="col4RptProfolio">
-                    <div className="BacimgDtls">
-                      <img
-                        alt="Lngo"
-                        src="/assets/images/protfolio/fornt-protfilo.webp"
-                      />
-                    </div>
-                    <div className="BxShow">
-                      <div className="BxMnvrSwn">
-                        <h5> Lorem Ipsum </h5>
-                        <Link className="text-primary" href={"/"}>
-                          {" "}
-                          Website Design{" "}
-                          <MdKeyboardArrowRight className=" text-base text-white inline-block" />
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="col4RptProfolio">
-                    <div className="BacimgDtls">
-                      <img
-                        alt="Lngo"
-                        src="/assets/images/protfolio/fornt-protfilo.webp"
-                      />
-                    </div>
-                    <div className="BxShow">
-                      <div className="BxMnvrSwn">
-                        <h5> Lorem Ipsum </h5>
-                        <Link className="text-primary" href={"/"}>
-                          {" "}
-                          Website Design{" "}
-                          <MdKeyboardArrowRight className=" text-base text-white inline-block" />
-                        </Link>
-                      </div>
-                    </div>
+                  <div>
+                    <img src="/assets/images/portfolio/portfoliomont.png" />
                   </div>
                 </div>
               </div>
             </>
           ) : select == 3 ? (
             <>
-              <div className="PortifloClum">
-                <div className="Protfolicol-12">
-                  <div className="col4RptProfolio">
-                    <div className="BacimgDtls">
-                      <img
-                        alt="Lngo"
-                        src="/assets/images/protfolio/fornt-protfilo.webp"
-                      />
+              <div className="container-ack gradientbackgroundone px-12 pt-12">
+                <div className=" grid grid-cols-3 justify-between ">
+                  <div className="col-span-2">
+                    <h2 className="text-xl font-semibold ">
+                      The standard Lorem Ipsum passage, used since the 1500s
+                    </h2>
+                    <p className="">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.
+                    </p>
+                  </div>
+                  <div>
+                    <div className="flex justify-end pb-10 col-span-1">
+                      <Link href="#">
+                        <button className=" button-2  px-7 py-2 rounded-md">
+                          <div className="eff-2"></div>
+                          <span className="text-base flex items-center ">
+                            {" "}
+                            View All
+                            <MdKeyboardArrowRight className=" text-base" />
+                          </span>
+                        </button>
+                      </Link>
                     </div>
-                    <div className="BxShow">
-                      <div className="BxMnvrSwn">
-                        <h5> Lorem Ipsum </h5>
-                        <Link className="text-primary" href={"/"}>
-                          {" "}
-                          Website Design{" "}
-                          <MdKeyboardArrowRight className=" text-base text-white inline-block" />
+                  </div>
+                </div>
+                <div>
+                  <img src="/assets/images/portfolio/portfoliomile.png" />
+                </div>
+              </div>
+              <div className="mt-12">
+                <div className="container-ack gradientbackgroundtwo px-12 pt-12 ">
+                  <div className=" grid grid-cols-3 justify-between ">
+                    <div className="col-span-2">
+                      <h2 className="text-xl font-semibold ">
+                        The standard Lorem Ipsum passage, used since the 1500s
+                      </h2>
+                      <p className="">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua.
+                      </p>
+                    </div>
+                    <div>
+                      <div className="flex justify-end pb-10 col-span-1">
+                        <Link href="#">
+                          <button className=" button-2  px-7 py-2 rounded-md">
+                            <div className="eff-2"></div>
+                            <span className="text-base flex items-center ">
+                              {" "}
+                              View All
+                              <MdKeyboardArrowRight className=" text-base" />
+                            </span>
+                          </button>
                         </Link>
                       </div>
                     </div>
                   </div>
-
-                  <div className="col4RptProfolio">
-                    <div className="BacimgDtls">
-                      <img
-                        alt="Lngo"
-                        src="/assets/images/protfolio/fornt-protfilo.webp"
-                      />
-                    </div>
-                    <div className="BxShow">
-                      <div className="BxMnvrSwn">
-                        <h5> Lorem Ipsum </h5>
-                        <Link className="text-primary" href={"/"}>
-                          {" "}
-                          Website Design{" "}
-                          <MdKeyboardArrowRight className=" text-base text-white inline-block" />
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="col4RptProfolio">
-                    <div className="BacimgDtls">
-                      <img
-                        alt="Lngo"
-                        src="/assets/images/protfolio/fornt-protfilo.webp"
-                      />
-                    </div>
-                    <div className="BxShow">
-                      <div className="BxMnvrSwn">
-                        <h5> Lorem Ipsum </h5>
-                        <Link className="text-primary" href={"/"}>
-                          {" "}
-                          Website Design{" "}
-                          <MdKeyboardArrowRight className=" text-base text-white inline-block" />
-                        </Link>
-                      </div>
-                    </div>
+                  <div>
+                    <img src="/assets/images/portfolio/portfoliomont.png" />
                   </div>
                 </div>
               </div>
             </>
           ) : (
             <>
-              <div className="PortifloClum">
-                <div className="Protfolicol-12">
-                  <div className="col4RptProfolio">
-                    <div className="BacimgDtls">
-                      <img
-                        alt="Lngo"
-                        src="/assets/images/protfolio/fornt-protfilo.webp"
-                      />
+              <div className="container-ack gradientbackgroundone px-12 pt-12">
+                <div className=" grid grid-cols-3 justify-between ">
+                  <div className="col-span-2">
+                    <h2 className="text-xl font-semibold ">
+                      The standard Lorem Ipsum passage, used since the 1500s
+                    </h2>
+                    <p className="">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.
+                    </p>
+                  </div>
+                  <div>
+                    <div className="flex justify-end pb-10 col-span-1">
+                      <Link href="#">
+                        <button className=" button-2  px-7 py-2 rounded-md">
+                          <div className="eff-2"></div>
+                          <span className="text-base flex items-center ">
+                            {" "}
+                            View All
+                            <MdKeyboardArrowRight className=" text-base" />
+                          </span>
+                        </button>
+                      </Link>
                     </div>
-                    <div className="BxShow">
-                      <div className="BxMnvrSwn">
-                        <h5> Lorem Ipsum </h5>
-                        <Link className="text-primary" href={"/"}>
-                          {" "}
-                          Website Design{" "}
-                          <MdKeyboardArrowRight className=" text-base text-white inline-block" />
+                  </div>
+                </div>
+                <div>
+                  <img src="/assets/images/portfolio/portfoliomile.png" />
+                </div>
+              </div>
+              <div className="mt-12">
+                <div className="container-ack gradientbackgroundtwo px-12 pt-12 ">
+                  <div className=" grid grid-cols-3 justify-between ">
+                    <div className="col-span-2">
+                      <h2 className="text-xl font-semibold ">
+                        The standard Lorem Ipsum passage, used since the 1500s
+                      </h2>
+                      <p className="">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua.
+                      </p>
+                    </div>
+                    <div>
+                      <div className="flex justify-end pb-10 col-span-1">
+                        <Link href="#">
+                          <button className=" button-2  px-7 py-2 rounded-md">
+                            <div className="eff-2"></div>
+                            <span className="text-base flex items-center ">
+                              {" "}
+                              View All
+                              <MdKeyboardArrowRight className=" text-base" />
+                            </span>
+                          </button>
                         </Link>
                       </div>
                     </div>
                   </div>
-
-                  <div className="col4RptProfolio">
-                    <div className="BacimgDtls">
-                      <img
-                        alt="Lngo"
-                        src="/assets/images/protfolio/fornt-protfilo.webp"
-                      />
-                    </div>
-                    <div className="BxShow">
-                      <div className="BxMnvrSwn">
-                        <h5> Lorem Ipsum </h5>
-                        <Link className="text-primary" href={"/"}>
-                          {" "}
-                          Website Design{" "}
-                          <MdKeyboardArrowRight className=" text-base text-white inline-block" />
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="col4RptProfolio">
-                    <div className="BacimgDtls">
-                      <img
-                        alt="Lngo"
-                        src="/assets/images/protfolio/fornt-protfilo.webp"
-                      />
-                    </div>
-                    <div className="BxShow">
-                      <div className="BxMnvrSwn">
-                        <h5> Lorem Ipsum </h5>
-                        <Link className="text-primary" href={"/"}>
-                          {" "}
-                          Website Design{" "}
-                          <MdKeyboardArrowRight className=" text-base text-white inline-block" />
-                        </Link>
-                      </div>
-                    </div>
+                  <div>
+                    <img src="/assets/images/portfolio/portfoliomont.png" />
                   </div>
                 </div>
               </div>
