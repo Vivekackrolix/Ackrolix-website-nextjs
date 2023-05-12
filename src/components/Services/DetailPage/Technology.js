478ab1f2fdfc98f74cb0e585862842162bf0bd37
Aakif <akif@ackrolix.in>
2023-05-12 14:28:45 +0530

    AakifHere

--- src/components/Services/DetailPage/Technology.js
+++ src/components/Services/DetailPage/Technology.js
@@ -4,42 +4,42 @@ import ComponentHead from "../../Common/ComponentHead";
 const Technology = () => {
   const process = [
     {
-      id: "1",
-      src:"/assets/images/service/icon.png",
-      name: "Node.js",
-      desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
+        id: "1",
+        img: "/assets/images/service/node.svg",
+        name: "Node.js",
+        desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
     },
     {
-      id: "2",
-      src:"/assets/images/service/icon.png",
-      name: "React.js",
-      desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
+        id: "2",
+        img: "/assets/images/service/react.svg",
+        name: "React.js",
+        desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
     },
     {
-      id: "3",
-      src:"/assets/images/service/icon.png",
-      name: "Angular",
-      desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
-   },
-    {
-      id: "4",
-      src:"/assets/images/service/icon.png",
-      name: "Laravel",
-      desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
-   },
-    {
-      id: "5",
-      src:"/assets/images/service/icon.png",
-      name: "Typescript",
-      desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
-   },
-    {
-      id: "6",
-      src:"/assets/images/service/icon.png",
-      name: "Next.js",
-      desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
+        id: "3",
+        img: "/assets/images/service/angularjs.svg",
+        name: "Angular",
+        desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
     },
-  ];
+    {
+        id: "4",
+        img: "/assets/images/service/laravel.svg",
+        name: "Laravel",
+        desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
+    },
+    {
+        id: "5",
+        img: "/assets/images/service/typescript.svg",
+        name: "Typescript",
+        desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
+    },
+    {
+        id: "6",
+        img: "/assets/images/service/nextjs.svg",
+        name: "Next.js",
+        desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
+    },
+];
 
   return (
     <div className="container-ack md:px-0 px-5">
@@ -52,16 +52,14 @@ const Technology = () => {
       <div className="grid md:grid-cols-3 md:py-10  pt-16 gap-10">
         {process.map((item,e) => (
           <>
-            <div key={e} className="group  flex justify-start items-start border border-textcolor rounded-md p-3 hover:bg-gray-100 hover:border-transparent">
-              <img src={item.src} alt="" />
-              <div>
-                <h2 className="font-semibold text-lg pt-3 text-black ">
-                  {item.name}
-                </h2>
-
-                {/* <p className="text-textcolor py-2 pb-4 text-sm ">{item.desc}</p> */}
-              </div>
-            </div>
+            <div key={e} className="group  flex justify-start items-center border border-textcolor rounded-md p-3 hover:bg-gray-100 hover:border-transparent gap-3 cursor-pointer">
+                            <img src={item.img} alt="" className='w-[34px]' />
+                            <div>
+                                <span className="font-semibold text-lg pt-3 text-black ">
+                                    {item.name}
+                                </span>
+                            </div>
+                        </div>
           </>
         ))}
       </div>
--- src/pages/AppModernization/AppModTech.js
+++ src/pages/AppModernization/AppModTech.js
@@ -5,37 +5,37 @@ const AppModTech = () => {
     const process = [
         {
             id: "1",
-            src: "/assets/images/service/icon.png",
+            img: "/assets/images/service/node.svg",
             name: "Node.js",
             desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
         },
         {
             id: "2",
-            src: "/assets/images/service/icon.png",
+            img: "/assets/images/service/react.svg",
             name: "React.js",
             desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
         },
         {
             id: "3",
-            src: "/assets/images/service/icon.png",
+            img: "/assets/images/service/angularjs.svg",
             name: "Angular",
             desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
         },
         {
             id: "4",
-            src: "/assets/images/service/icon.png",
+            img: "/assets/images/service/laravel.svg",
             name: "Laravel",
             desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
         },
         {
             id: "5",
-            src: "/assets/images/service/icon.png",
+            img: "/assets/images/service/typescript.svg",
             name: "Typescript",
             desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
         },
         {
             id: "6",
-            src: "/assets/images/service/icon.png",
+            img: "/assets/images/service/nextjs.svg",
             name: "Next.js",
             desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
         },
@@ -48,18 +48,15 @@ const AppModTech = () => {
                 sub="Website Technology"
                 head="Whatever web applications you’re building, we got your back at every stage of web development, including web design. We’ve built a core system for a bank, an MVP marketplace sold for millions, and other 5☆ custom projects for 140+ companies worldwide."
             />
-
-            <div className="grid md:grid-cols-3 md:py-10  pt-16 gap-10">
+            <div className="grid md:grid-cols-3 md:py-10 pt-16 gap-10">
                 {process.map((item, e) => (
                     <>
-                        <div key={e} className="group  flex justify-start items-start border border-textcolor rounded-md p-3 hover:bg-gray-100 hover:border-transparent">
-                            <img src={item.src} alt="" />
+                        <div key={e} className="group  flex justify-start items-center border border-textcolor rounded-md p-3 hover:bg-gray-100 hover:border-transparent gap-3 cursor-pointer">
+                            <img src={item.img} alt="" className='w-[34px]' />
                             <div>
-                                <h2 className="font-semibold text-lg pt-3 text-black ">
+                                <span className="font-semibold text-lg pt-3 text-black ">
                                     {item.name}
-                                </h2>
-
-                                {/* <p className="text-textcolor py-2 pb-4 text-sm ">{item.desc}</p> */}
+                                </span>
                             </div>
                         </div>
                     </>
