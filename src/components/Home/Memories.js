ab25c60e401e9d4ad907e08e3cddacf1f7736825
Misthitweety <sweeti@ackrolix.in>
2023-05-20 10:58:06 +0530

    [Update: memories]

--- src/components/Home/Memories.js
+++ src/components/Home/Memories.js
@@ -1,14 +1,10 @@
 import React, { useState } from "react";
-import { MdKeyboardArrowRight } from "react-icons/md";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { BiChevronLeft } from "react-icons/bi";
-import { BiChevronRight } from "react-icons/bi";
 import Link from "next/link";
+import Slider from "react-slick";
 import ComponentHead from "../Common/ComponentHead";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 
-const Memories = () => {
+function Memories() {
   const resumeData = [
     { id: 1, sliderImg: "assets/images/memories/home-gallery/1.webp" },
     { id: 2, sliderImg: "assets/images/memories/home-gallery/2.webp" },
@@ -17,47 +13,37 @@ const Memories = () => {
     { id: 5, sliderImg: "assets/images/memories/home-gallery/2.webp" },
   ];
 
-  const PrevArrow = ({ onClick }) => {
-    return (
-      <div
-        className="arrow prev cursor-pointer text-5xl absolute sm:top-[250px] bg-white text-black border-[1px]  prevarrow rounded-[50%] sm:left-[29%] z-10"
-        onClick={onClick}
-      >
-        <BiChevronLeft />
-      </div>
-    );
-  };
-
   const NextArrow = ({ onClick }) => {
     return (
-      <div
-        className="arrow next text-5xl absolute sm:top-[250px] bg-white cursor-pointer text-black border-[1px] rounded-[50%] nextarrow sm:right-[29%] z-10"
-        onClick={onClick}
-      >
-        <BiChevronRight />
+      <div className="arrow next" onClick={onClick}>
+        <FaArrowRight />
       </div>
     );
   };
 
-  const [imgslide, setImgslide] = useState(0);
+  const PrevArrow = ({ onClick }) => {
+    return (
+      <div className="arrow prev" onClick={onClick}>
+        <FaArrowLeft />
+      </div>
+    );
+  };
+
+  const [imgIndex, setImgIndex] = useState(0);
 
   const settings = {
-    dots: false,
-    autoplay: true,
-    className: "center",
-    loop: true,
     infinite: true,
+    className: "center",
     lazyLoad: true,
-    centerMode: true,
-    centerPadding: "0px",
-    speed: 100,
-    autoplaySpeed: 2500,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 4000,
     slidesToShow: 3,
     slidesToScroll: 1,
-    accessibility: true,
-    cssEase: "ease-out",
-    // prevArrow: <PrevArrow />,
+    centerMode: true,
+    centerPadding: 0,
     // nextArrow: <NextArrow />,
+    // prevArrow: <PrevArrow />,
     responsive: [
       {
         breakpoint: 1080,
@@ -95,7 +81,7 @@ const Memories = () => {
         },
       },
     ],
-    beforeChange: (current, next) => setImgslide(next),
+    beforeChange: (current, next) => setImgIndex(next),
   };
 
   return (
@@ -105,44 +91,30 @@ const Memories = () => {
         sub="Life At Ackrolix"
         head="Creating Memories that reminds: Ackrolix Innovations "
       />
-      <div className="mt-2">
-        <div className="text-white mx-auto">
-          <div className="absolute md:right-20 right-[5%] cursor-pointer top-10 z-20 md:top-10"></div>
-          <div className="pt-15 MemoriesSlider">
-            <Slider {...settings}>
-              {resumeData.map((item, indx) => {
-                return (
-                  <div
-                    key={indx}
-                    className={
-                      "scale-185 SldrMemories rounded-[20px]"
-                    }
-                  >
-                    <img
-                      alt="Slider"
-                      src={item.sliderImg}
-                      className={"rounded-[20px] md:w-full mx-auto"}
-                    />
-                  </div>
-                );
-              })}
-            </Slider>
-          </div>
-        </div>
+
+      <div className="MemoriesSlider">
+        <Slider {...settings}>
+          {resumeData.map((item, idx) => (
+            <div className={idx === imgIndex ? "m_slide activeSlide" : "m_slide"} key={idx}>
+              <img src={item.sliderImg} alt={idx} />
+            </div>
+          ))}
+        </Slider>
       </div>
 
       <div className="flex justify-center pt-6 ">
         <Link href="/moments">
-          <button className=" button-2  px-12 py-2 rounded-md">
+          <button className="button-2 px-12 py-2 rounded-md">
             <div className="eff-2"></div>
-            <span className="text-base flex items-center "> View All
-              <MdKeyboardArrowRight className=" text-base" />
+            <span className="text-base flex items-center ">
+              View All
+              <FaArrowRight className="text-base" />
             </span>
           </button>
         </Link>
       </div>
     </div>
   );
-};
+}
 
 export default Memories;
