e048119ef30da1806bba7913a276bd70e0c19366
Anjali1sh <anjali@ackrolix.com>
2023-07-12 14:41:14 +0530

    changes

--- src/components/Home/Memories.js
+++ src/components/Home/Memories.js
@@ -85,7 +85,7 @@ function Memories() {
   };
 
   return (
-    <div className="container-ack px-5 md:pt-0 pt-10 w-full relative">
+    <div className="container-ack px-5  w-full relative md:pt-0 pt-12">
       <ComponentHead
         title="Moments"
         sub="Life At Ackrolix"
