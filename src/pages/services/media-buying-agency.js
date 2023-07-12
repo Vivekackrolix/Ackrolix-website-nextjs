628e6401426e61e538a3bbe1b68dbec31822b649
Anjali1sh <anjali@ackrolix.com>
2023-07-12 10:53:56 +0530

    digital

--- src/pages/services/media-buying-agency.js
+++ src/pages/services/media-buying-agency.js
@@ -16,74 +16,173 @@ const MediaBuying = () => {
   const processes = [
     {
       id: "1",
-      name: "Rehosting or Lift-and-Shift",
-      desc: "This approach involves moving an existing application to a new infrastructure without making significant changes to the application's architecture or functionality.",
+      name: "We believe in the outcome",
+      desc: "As one of the few integrated social media buying agencies, we create a goal-specific plan for both branding and performance campaigns."
     },
     {
       id: "2",
-      name: "Refactoring or Re-architecting",
-      desc: "Refactoring involves making structural changes to the application's codebase and architecture while preserving its core functionality. It aims to improve the application's performance, scalability, and maintainability.",
+      name: "Pro at process",
+      desc: "Among social media buying agencies, we have a laser-sharp focus on creating ROI-driven campaigns by creating the most relevant custom audience and lookalikes."
     },
     {
       id: "3",
-      name: "Replatforming",
-      desc: "Replatforming entails moving an application to a different platform or technology stack while retaining its core features & functionality. For example, migrating an on-premises application to a cloud platform.",
+      name: "Defining strategies",
+      desc: "We execute negotiation strategies to optimize costs for brand visibility and leads, by varied means of media staging, ensuring your brand is top-notch at optimized costs."
     },
     {
       id: "4",
-      name: "Rearchitecting or Redesigning",
-      desc: "Rearchitecting involves a significant overhaul of the application's architecture and design to meet current business needs and technological advancements.",
+      name: "Creative media plans",
+      desc: "Our creative edge sets our media plan apart from other media companies by analyzing gaps to engage consumers through data-driven and targeted media plans at extremely competitive rates"
     },
     {
       id: "5",
-      name: "Containerization",
-      desc: "Containerization involves encapsulating an application and its dependencies into containers, using technologies like Docker. This approach allows applications to run consistently across different environments and simplifies deployment and scalability.",
+      name: "Innovation never goes out of fashion",
+      desc: "At GOZOOP, we believe in trying new platforms and strategies to achieve our targets to reach new conclusions and widen the horizon for revenue. We are always ready to push the boundaries of possibility to meet our goals."
+    },
+
+  ];
+  const techs = [
+    {
+      id: "1",
+      img: "/assets/images/service/svg/google.svg",
+      name: "Google",
+
+    },
+    {
+      id: "2",
+      img: "/assets/images/service/svg/google-ads.svg",
+      name: "Google Ads",
+
+    },
+    {
+      id: "3",
+      img: "/assets/images/service/svg/google-analytics.svg",
+      name: "google Analytics",
+
+    },
+    {
+      id: "4",
+      img: "/assets/images/service/svg/facebook.svg",
+      name: "Facebook",
+
+    },
+    {
+      id: "5",
+      img: "/assets/images/service/svg/instagram.svg",
+      name: "Instagram",
+
     },
     {
       id: "6",
-      name: "Replacement or Rebuilding",
-      desc: "In some cases, it may be more feasible to replace an existing application with a new solution rather than attempting to modernize it. This approach involves rebuilding the application from scratch, leveraging modern technologies and design principles.",
+      img: "/assets/images/service/svg/twitter.svg",
+      name: "Twitter",
+
     },
+    {
+      id: "7",
+      img: "/assets/images/service/svg/linkedin.svg",
+      name: "LinkedIn",
+
+    },
+    {
+      id: "8",
+      img: "/assets/images/service/svg/youtube.svg",
+      name: "YouTube",
+
+    },
+    {
+      id: "10",
+      img: "/assets/images/service/svg/pinterest.svg",
+      name: "Pinterest",
+
+    },
+    {
+      id: "11",
+      img: "/assets/images/service/svg/snapchat.svg",
+      name: "Snapchat",
+
+    },
+    {
+      id: "12",
+      img: "/assets/images/service/svg/tiktok.svg",
+      name: "TikTok",
+
+    },
+    {
+      id: "13",
+      img: "/assets/images/service/svg/quora.svg",
+      name: "Quora",
+
+    },
+    {
+      id: "14",
+      img: "/assets/images/service/svg/reddit.svg",
+      name: "Reddit",
+
+    },
+    {
+      id: "15",
+      img: "/assets/images/service/svg/mailchimp.png",
+      name: "Mailchimp",
+
+    },
+    {
+      id: "16",
+      img: "/assets/images/service/svg/semr.png",
+      name: "SEMrush",
+
+    },
+    {
+      id: "17",
+      img: "/assets/images/service/svg/ahref.png",
+      name: "Ahrefs",
+
+    },
+    {
+      id: "18",
+      img: "/assets/images/service/svg/moz.png",
+      name: "Moz",
+
+    },
+    {
+      id: "19",
+      img: "/assets/images/service/svg/analytics.webp",
+      name: "Adobe Analytics",
+
+    },
+    {
+      id: "20",
+      img: "/assets/images/service/svg/cj.png",
+      name: "Commission Junction",
+
+    },
+    {
+      id: "21",
+      img: "/assets/images/service/svg/shareasale.png",
+      name: "ShareASale",
+
+    },
+    {
+      id: "22",
+      img: "/assets/images/service/svg/hubspot.png",
+      name: "HubSpot",
+
+    },
+    {
+      id: "23",
+      img: "/assets/images/service/svg/pardot.png",
+      name: "Pardot",
+
+    },
+    {
+      id: "24",
+      img: "/assets/images/service/svg/marketo.png",
+      name: "Marketo",
+
+    },
+   
   ];
-const techs = [
-    {
-        id: "1",
-        img: "/assets/images/service/node.svg",
-        name: "Node.js",
-        desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
-    },
-    {
-        id: "2",
-        img: "/assets/images/service/react.svg",
-        name: "React.js",
-        desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
-    },
-    {
-        id: "3",
-        img: "/assets/images/service/angularjs.svg",
-        name: "Angular",
-        desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
-    },
-    {
-        id: "4",
-        img: "/assets/images/service/laravel.svg",
-        name: "Laravel",
-        desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
-    },
-    {
-        id: "5",
-        img: "/assets/images/service/typescript.svg",
-        name: "Typescript",
-        desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
-    },
-    {
-        id: "6",
-        img: "/assets/images/service/nextjs.svg",
-        name: "Next.js",
-        desc: "Lorem ipsum dolor sit amet consect Sapien porttitor. ",
-    },
-];
-const cards = [
+  const cards = [
     {
       id: 1,
       para: "Our modernization solutions revolutionize your applications' performance, scalability, agility, & maintainability. We enable seamless integration with emerging technologies, optimize infrastructure, & streamline workflows to ensure your applications are future-proof.",
@@ -172,11 +271,11 @@ const cards = [
       textcolor: "text-secondary",
       main: "hidden",
     },
-  ];  
+  ];
   return (
     <div>
       <Head>
-        <title>Media Buying and Plannig</title>        
+        <title>Media Buying and Plannig</title>
       </Head>
       <Header />
       <div
@@ -185,23 +284,27 @@ const cards = [
           backgroundImage: 'url("/assets/images/mainbackground.png ")',
         }}
       >
-        <Banner 
-          bannertitle="Make your systems efficient & scalable with our app modernization expertise"
-          bannersubtext="Get your app back on track for growth with our practice-driven app modernization skills. We provide full-scope app modernization services you can start using quickly to fix, boost, and revamp your systems with teams that have worked for over 160 companies."
+        <Banner
+          bannertitle="We know exactly where to place your ads and we’re equipped with all the right tools!"
+          // bannersubtext="Get your app back on track for growth with our practice-driven app modernization skills. We provide full-scope app modernization services you can start using quickly to fix, boost, and revamp your systems with teams that have worked for over 160 companies."
           serviceimg="/assets/images/service/svg/buy.png"
-          servicehead="Put your present in order so you can focus on scaling up for new users"
-          servicedesc1="Popular apps often fall victim to their own success. They grow fast to satisfy business requirements, patched up ad-hoc by different teams. Eventually, they become sluggish and difficult to maintain, let alone extend and improve. App modernization can turn it all around."
-          servicedesc2="The Ackrolix Innovations development teams have been called to the rescue of many legacy projects. In their course, our developers carried out tech stack migrations and consolidations, refactored codebases, designed and redesigned architectures, moved from a monolith to microservices, and performed architecture audits."
+          servicehead="MEDIA PLANNING BUYING "
+          servicedesc1="Optimizing platforms, pushing the boundaries of innovation, and engaging with consumers is what we do.
+
+          As a digital media agency, we drive outcomes for brands and map the right audiences within the digital landscape. From communication planning, performance planning to e-commerce consulting, our media team is powered by data-led insights, rooted in human behavior."
+          servicedesc2="We provide a 360-degree plan within the digital fraternity, including social, third-party, affiliate marketing, and native ads.
+
+          We are not only a programmatic buying agency but also among the best PPC advertising companies available. Our team brings together creative thinking, and a lens for innovation, remaining focused on constantly driving value for partner brands through a spectrum of services."
         />
       </div>
-      <Cards card={cards} />
+      {/* <Cards card={cards} /> */}
       <div className="container-ack md:px-0 px-5">
-       <ComponentHead
-        title="Platforms"
-        sub="Media Buying and Plannig"
-        head="The Platforms stack for Media Buying and Plannig can vary depending on the specific requirements of the application and the chosen modernization approach."
-      />
-      <Technology tech={techs} />
+        <ComponentHead
+          title="Platforms"
+          sub="Media Buying and Plannig"
+          head="The Platforms stack for Media Buying and Plannig can vary depending on the specific requirements of the application and the chosen modernization approach."
+        />
+        <Technology tech={techs} />
       </div>
 
       <div>
@@ -214,8 +317,8 @@ const cards = [
           <div className="container-ack md:px-0 px-5">
             <ProcessHead
               processhead="Services"
-              processsub="App Modernization On Peek"
-              processdesc="Full-scope app modernization services you can start using quickly (even in weeks!) to fix, boost, and revamp your systems with teams that have worked for over 160 companies."
+              processsub="Media Buying and Plannig"
+              processdesc="Optimizing platforms, pushing the boundaries of innovation."
               processimg="/assets/images/service/svg/buy1.png"
             />
             <Process process={processes} />
