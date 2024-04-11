ec4786d3320e19afd1014912c592f967b2ff9938
fastestsunil <sunil141292@gmail.com>
2024-04-11 22:07:58 +0530

    faq added

--- src/pages/blogs/[slug].js
+++ src/pages/blogs/[slug].js
@@ -2,10 +2,10 @@ import Head from 'next/head';
 import React from 'react';
 import Header from '../../components/Common/Header';
 import Heading from '../../components/Common/Heading';
-import Bloglist from '../../components/Blogs/BlogList';
 import Footer from '../../components/Common/Footer';
-import { allPosts, Post } from 'contentlayer/generated';
-
+import { allPosts } from 'contentlayer/generated';
+import { Disclosure } from '@headlessui/react';
+import { FiChevronRight } from 'react-icons/fi';
 export const getStaticPaths = () => {
   const paths = allPosts.map(post => `/blogs/${post.slug}`);
   return { paths, fallback: false };
@@ -18,6 +18,7 @@ export const getStaticProps = context => {
 };
 
 export default function Blog({ post }) {
+  console.log(post.faqs);
   return (
     <div>
       <Header />
@@ -34,12 +35,47 @@ export default function Blog({ post }) {
       </div>
       <div className="px-5 [&_.prose]:max-w-[100ch] container-ack md:py-12 md:px-0">
         <article
-          class="prose  lg:prose-xl
+          className="prose  lg:prose-xl
             [&_h1]:text-4xl [&_h2]:text-2xl [&_h3]:text-xl [&_h4]:text-lg [&_h5]:text-base [&_h6]:text-sm
         "
         >
           <div dangerouslySetInnerHTML={{ __html: post?.body?.html }} />
         </article>
+
+        <div className="max-w-xl mx-auto">
+          <div className="flex items-center justify-center mb-5 text-xl font-semibold">
+            FAQ
+          </div>
+          {post?.faqs?.map((faq, index) => {
+            return (
+              <div className="rounded-sm" key={index}>
+                <Disclosure>
+                  {({ open }) => (
+                    <>
+                      <Disclosure.Button
+                        className={`px-3 mb-3 ${
+                          open ? 'bg-gray-200' : 'bg-gray-100 '
+                        } flex items-center w-full gap-2 py-1`}
+                      >
+                        <p className="flex-1 text-lg text-left">
+                          {faq?.question}
+                        </p>
+                        <FiChevronRight
+                          className={`shrink-0 ${
+                            open ? 'rotate-90 transform' : ''
+                          }`}
+                        />{' '}
+                      </Disclosure.Button>
+                      <Disclosure.Panel className="text-gray-500">
+                        <p className="px-3 pb-4">{faq?.answer}</p>
+                      </Disclosure.Panel>
+                    </>
+                  )}
+                </Disclosure>
+              </div>
+            );
+          })}
+        </div>
       </div>
 
       <Footer />
